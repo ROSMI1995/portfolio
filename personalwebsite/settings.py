59073ed9d3b7463effe6a63aa7bc2ae6021ec404
ROSMI1995 <rosmithomas212@gmail.com>
2022-10-11 10:22:09 +0530

    first commit

--- personalwebsite/settings.py
+++ personalwebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 import os
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -121,7 +122,10 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'website')]
 STATIC_ROOT = os.path.join (BASE_DIR, 'static')
 
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
